2aead9c580454d910493735ab80bf40d01bc266d
imPBH <alex.provo31@gmail.com>
2022-09-21 09:07:59 +0200

    BattleState

---
+++ BattleState.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Project_CS
+{
+    public class BattleState : IState
+    {
+        private PlayerController context;
+
+        public BattleState(PlayerController context)
+        {
+            this.context = context;
+        }
+
+        public int Battle()
+        {
+            Console.WriteLine("You try to kill the enemy");
+            return 0;
+        }
+
+        public int Explore()
+        {
+            Console.WriteLine("You can't explore while in combat!");
+            return 0;
+        }
+    }
+}
