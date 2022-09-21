caee6fb448de05017f0250aebe59404b5ccc462c
imPBH <alex.provo31@gmail.com>
2022-09-21 11:49:07 +0200

    Added Discord Nitro

---
+++ Loot/DiscordNitro.cs
@@ -0,0 +1,23 @@
+using System;
+using Project_CS.Player;
+
+namespace Project_CS.Loot
+{
+    public class DiscordNitro : ILoot
+    {
+        public string Name { get; } = "Discord Nitro";
+        public int Attack { get; } = 2;
+        public int Defense { get; } = 0;
+
+        public void Use(PlayerController player)
+        {
+            Console.WriteLine("You used Discord Nitro!");
+        }
+        
+        public void Break(PlayerController player)
+        {
+            Console.WriteLine("Your Discord Nitro membership has expired!");
+            player.RemoveLoot(this);
+        }
+    }
+}
