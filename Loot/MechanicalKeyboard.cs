ae2ee99e0d53424392992d80746a1ebf081d7951
imPBH <alex.provo31@gmail.com>
2022-09-21 12:05:56 +0200

    Added Mechanical Keyboard

---
+++ Loot/MechanicalKeyboard.cs
@@ -0,0 +1,23 @@
+using System;
+using Project_CS.Player;
+
+namespace Project_CS.Loot
+{
+    public class MechanicalKeyboard : ILoot
+    {
+        public string Name { get; } = "Mechanical Keyboard";
+        public int Attack { get; } = 1;
+        public int Defense { get; } = 0;
+
+        public void Use(PlayerController player)
+        {
+            Console.WriteLine("You use the Mechanical Keyboard");
+        }
+
+        public void Break(PlayerController player)
+        {
+            Console.WriteLine("You broke the Mechanical Keyboard");
+            player.RemoveLoot(this);
+        }
+    }
+}
