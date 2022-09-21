0cbce5a0fdc09c31da8ae5bffb2b090cd0e9a7fd
imPBH <alex.provo31@gmail.com>
2022-09-21 09:24:42 +0200

    ExecuteChoice method, added explore

--- GameController.cs
+++ GameController.cs
@@ -7,5 +7,18 @@ namespace Project_CS
         static PlayerController player = new PlayerController();
         static int score;
         static int nextLevel = 10;
+
+        private static void ExecuteChoice(ConsoleKey key)
+        {
+            if (key == ConsoleKey.L)
+            {
+                int points = player.Explore();
+                if (points > 0)
+                {
+                    Console.WriteLine("You gained " + points + " points!");
+                    score += points;
+                }
+            }
+        }
     }
 }
