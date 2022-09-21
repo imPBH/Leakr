70c2cdbe78df74e62d8733d2f87890a7265f52de
imPBH <alex.provo31@gmail.com>
2022-09-21 11:49:20 +0200

    Added Deodorant

---
+++ Loot/Deodorant.cs
@@ -0,0 +1,24 @@
+using Project_CS.Player;
+using System;
+
+namespace Project_CS.Loot
+{
+    public class Deodorant : ILoot
+    {
+        public string Name { get; } = "Deodorant";
+        public int Attack { get; } = 0;
+        public int Defense { get; } = 2;
+
+        public void Use(PlayerController player)
+        {
+            Console.WriteLine("You smell better now.");
+        }
+
+        public void Break(PlayerController player)
+        {
+            Console.WriteLine("No more deodorant.");
+            player.RemoveLoot(this);
+        }
+        
+    }
+}
