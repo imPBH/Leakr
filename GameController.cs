a5843b801eb736747f7c10a8649b2ab507dcfccc
imPBH <alex.provo31@gmail.com>
2022-09-21 09:25:00 +0200

    ExecuteChoice method, added battle

--- GameController.cs
+++ GameController.cs
@@ -19,6 +19,21 @@ namespace Project_CS
                     score += points;
                 }
             }
+            else if (key == ConsoleKey.A)
+            {
+                int rounds = player.Battle();
+                if (rounds > 0)
+                {
+                    int points = 10 - rounds;
+                    if (points < 0)
+                    {
+                        points = 0;
+                    }
+
+                    score += points;
+                    Console.WriteLine("You gained " + points + " points!");
+                }
+            }
         }
     }
 }
