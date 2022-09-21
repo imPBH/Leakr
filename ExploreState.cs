04ebd56bbf56fd4b07f505b08ec7cc979d9e5ff6
imPBH <alex.provo31@gmail.com>
2022-09-21 09:06:44 +0200

    ExploreState

---
+++ ExploreState.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Project_CS
+{
+    public class ExploreState : IState
+    {
+        private PlayerController context;
+
+        public ExploreState(PlayerController context)
+        {
+            this.context = context;
+        }
+
+        public int Battle()
+        {
+            Console.WriteLine("There is nothing to attack");
+            return 0;
+        }
+
+        public int Explore()
+        {
+            Console.WriteLine("Looking around...");
+            return 0;
+        }
+    }
+}
