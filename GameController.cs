486cb370669a06d2a05b937f35ac748af63dce54
imPBH <alex.provo31@gmail.com>
2022-09-21 09:20:18 +0200

    GameController Attributes

---
+++ GameController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Project_CS
+{
+    public class GameController
+    {
+        static PlayerController player = new PlayerController();
+        static int score;
+        static int nextLevel = 10;
+    }
+}
