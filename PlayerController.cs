cc9f0e01c3fbaf61a10c4988d80a4147721a0b3b
imPBH <alex.provo31@gmail.com>
2022-09-21 08:57:15 +0200

    Player controller

---
+++ PlayerController.cs
@@ -0,0 +1,20 @@
+﻿namespace Project_CS
+{
+    public class PlayerController
+    {
+        private IState currentState;
+        private IState exploreState;
+        private IState batttleState;
+        private int level;
+        private int exp;
+        private int health;
+        private string name = "";
+
+        public PlayerController()
+        {
+            exploreState = new ExploreState(this);
+            batttleState = new BattleState(this);
+            currentState = exploreState;
+        }
+    }
+}
